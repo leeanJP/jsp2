63ca84ef00a21c82e778c473c81a5a74648a9f12
leean <zzzttt9@gmail.com>
2023-12-21 14:55:42 +0900

    git test 04

--- src/main/webapp/js/sample.js
+++ src/main/webapp/js/sample.js
@@ -1,19 +1,8 @@
 var INDEX = {};
 
 INDEX.init = function () {
-    INDEX.initTemplate();
-    
+   console.log("test111");
 
 
-
-}
-
-
-INDEX.initTemplate = function (){
-    //시작 템플릿 스크립트
-    //grid, 필요 데이터 불러오기 등등
-}
-
-INDEX.함수생성 = function (){
 
 }
